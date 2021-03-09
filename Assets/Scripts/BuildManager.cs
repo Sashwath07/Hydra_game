b54cc88dbba0a481a4653d849894e6c74832562a
sashwath <sashwath.ravilla@gmail.com>
2021-03-09 14:09:25 +0800

    First commit
    First commit

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake(){
+        if (instance != null){
+            Debug.Log("More that one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }  
+
+    public GameObject standardTurretPrefab;
+    public GameObject missileLauncherPrefab;
+
+    public GameObject buildEffect;
+    
+    private TurretBlueprint turretToBuild;
+
+    public bool CanBuild {get {return turretToBuild != null;}}
+    public bool HasMoney {get {return PlayerStats.Money >= turretToBuild.cost;}}
+
+    public void BuildTurretOn (Node node){
+        if (PlayerStats.Money < turretToBuild.cost){
+            Debug.Log("Not enough money!");
+            return;
+        }
+        PlayerStats. Money -= turretToBuild.cost;
+
+       GameObject turret =  (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
+       node.turret = turret;
+
+       GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+       Destroy(effect, 5f);
+
+       Debug.Log("Turret Built! Money left: " + PlayerStats.Money);
+    }
+
+    public void SelectTurretToBuild(TurretBlueprint turret){
+        turretToBuild = turret;
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private bool doMovement = true;
+
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            doMovement = !doMovement;
+        }
+
+        if (!doMovement){
+            return;
+        }
+
+        var direction = Input.inputString;
+
+        switch (direction){
+            
+            case "w": 
+                transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+                break;
+            case "s":
+                transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+                break;
+            case "d":
+                transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+                break;
+            case "a":
+                transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+                break;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        
+        Vector3 pos = transform.position;
+        pos.y -= scroll * 100 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        transform.position = pos;
+    }
+}
