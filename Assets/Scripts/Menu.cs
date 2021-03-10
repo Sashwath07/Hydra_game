b8bd07f59d6e5a5f94b6606335b6d2bb14cdf9ac
KooJianYang <74820582+KooJianYang@users.noreply.github.com>
2021-03-10 23:15:38 +0800

    Add files via upload

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button StoryModeButton;
+    public Button PVPButton;
+    public Button LeaderboardButton;
+    public Button LogoutButton;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoryModeButton.onClick.AddListener(() => {
+            //add purpose of storymode button
+        });
+
+        PVPButton.onClick.AddListener(() => {
+            //add purpose of storymode button
+        });
+
+        LeaderboardButton.onClick.AddListener(() => {
+            //add purpose of storymode button
+        });
+
+        LogoutButton.onClick.AddListener(() => {
+            SceneManager.LoadScene("Login Scene", LoadSceneMode.Single);
+        });
+    }
+}
---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour
+{
+    public InputField NewUsernameInput;
+    public InputField NewPasswordInput;
+    public InputField ConfirmPasswordInput;
+    public Button RegisterAccountButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RegisterAccountButton.onClick.AddListener(() => {
+            StartCoroutine(Main.Instance.Web.Register(NewUsernameInput.text, NewPasswordInput.text, ConfirmPasswordInput.text));
+        });
+    }
+}
