82cc32fb3a35c77b29094fa206fe6e02f80136f6
KooJianYang <jianyangkoo@gmail.com>
2021-03-22 16:40:10 +0800

    deleting obsolete scripts
    Web.cs and Main.cs are obsolete as previously i used localhost to fill as database. now we dont need it anymore.
    
    Register.cs is fully commented and will be edited ltr to accomodate new database

--- Assets/Scripts/Register.cs
+++ Assets/Scripts/Register.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class Register : MonoBehaviour
-{
-    public InputField NewUsernameInput;
-    public InputField NewPasswordInput;
-    public InputField ConfirmPasswordInput;
-    public Button RegisterAccountButton;
-    public Text RegisterFeedback;
+// public class Register : MonoBehaviour
+// {
+//     public InputField NewUsernameInput;
+//     public InputField NewPasswordInput;
+//     public InputField ConfirmPasswordInput;
+//     public Button RegisterAccountButton;
+//     public Text RegisterFeedback;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        RegisterAccountButton.onClick.AddListener(() => {
-            StartCoroutine(Main.Instance.Web.Register(NewUsernameInput.text, NewPasswordInput.text, ConfirmPasswordInput.text));
-        });
-    }
-}
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         RegisterAccountButton.onClick.AddListener(() => {
+//             StartCoroutine(Main.Instance.Web.Register(NewUsernameInput.text, NewPasswordInput.text, ConfirmPasswordInput.text));
+//         });
+//     }
+// }
